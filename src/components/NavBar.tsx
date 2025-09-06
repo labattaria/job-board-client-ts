90122ff9c0ac9f603276ec3f8ca017afa06765e6
labattaria <hallucinate322@gmail.com>
2025-09-06 21:30:12 +0300

    update Homepage path in header navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import { type User, logout } from '../lib/auth';
+import { Link } from "react-router-dom";
+import { type User, logout } from "../lib/auth";
 
 interface NavBarProps {
   user: User | null;
@@ -16,15 +16,13 @@ function NavBar({ user, onLogout }: NavBarProps) {
   return (
     <nav className="navbar">
       <div className="navbar-start">
-        <Link className="navbar-item" to="/job-board-client-ts/">
+        <Link className="navbar-item" to="/">
           Home
         </Link>
       </div>
       {loggedIn ? (
         <div className="navbar-end">
-          <span className="navbar-item has-text-grey">
-            {user.email}
-          </span>
+          <span className="navbar-item has-text-grey">{user.email}</span>
           <Link className="navbar-item" to="/jobs/new">
             Post Job
           </Link>
